34c62db89199fda4fd4e5aaaa579d7d6491db074
Ganya-MM <74894431+Ganya-MM@users.noreply.github.com>
2021-07-16 23:04:39 +0530

    Update productRepo.js

--- dbrepos/productRepo.js
+++ dbrepos/productRepo.js
@@ -3,7 +3,7 @@ const db = require('../config/mongodb');
 
 exports.add = (product,callback) => {
         const collection = db.getCollection();
-        collection.insertOne({ name: product.name, contact: product.contact, subjects: product.subjects, class: product.class, society: product.society, year: product.year})
+        collection.insertOne({ name: product.name, contact: product.contact, subjects: product.subjects, classs: product.classs, society: product.society, year: product.year})
             .then(() => {
                 console.log("Document inserted");
                 callback("ok")
